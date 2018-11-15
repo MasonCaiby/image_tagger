96ef58dde2f5b0bceb7df8d8b3bf2ac7b24ca6a7
MasonCaiby <maxwell.caudle@gmail.com>
2018-11-15 14:32:03 -0700

    first commit

---
+++ image_tagger.py
@@ -0,0 +1,149 @@
+import cv2
+import numpy as np
+from image_vars import colors
+
+def add_banner(image_source, cv2_window):
+
+    image = cv2.imread(image_source)
+    height, width, channel = image.shape
+
+    final_image = np.zeros((height+100, width, 3), dtype=np.uint8)
+    final_image[100:, :, :] = image
+
+    return final_image
+
+def tag_image(image_source, colors, image_dict, cv2_window):
+    global ref_point, image, clone, ref_points, line_width
+
+    try:
+        image = cv2.imread(image_source)
+        height, width, channel = image.shape
+        original = image.copy()
+    except Exception as e:
+        return None, None
+
+    line_width = 3
+
+    cv2.setMouseCallback(cv2_window, click_and_crop)
+
+    ref_point = []
+
+
+    if image_dict:
+        ref_points = image_dict['ref_points']
+        for ref_point in ref_points:
+            cv2.rectangle(image, ref_point[0], ref_point[1], colors[ref_point[2]], line_width)
+    else:
+        ref_points = []
+
+    clone = image.copy()
+
+    while True:
+        # display the image and wait for a keypress
+        cv2.imshow(cv2_window, image)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key >= 48 and key < 58:
+            if len(ref_point) < 2:
+                print("Hmmm looks like you are trying to define a class while"
+                      " mouse is pressed down")
+                continue
+            input_class = key - 48
+            try:
+                ref_point[2] = input_class
+            except:
+                ref_point.append(input_class)
+            if ref_point not in ref_points:
+                ref_points.append(ref_point)
+            print(ref_points)
+
+            # draw a rectangle around the region of interest
+            rect_color = colors[input_class]
+            cv2.rectangle(clone, ref_point[0], ref_point[1], rect_color, line_width)
+            cv2.imshow(cv2_window, clone)
+            image = clone.copy()
+        # if the 'r' key is pressed, reset the cropping region
+        if key == ord("r"):
+            ref_points = []
+            image = original.copy()
+            clone = original.copy()
+
+        elif key == ord("x"):
+            print('removing ', rect)
+            ref_points.remove(rect)
+
+        # if the 'q' key is pressed, break from the loop
+        elif key == ord("q"):
+            image_dict = {'width' : width,
+                          'height': height,
+                          'ref_points': ref_points}
+            return image_dict, 'quit'
+
+        elif key == ord(" "):
+            if image_dict == None:
+                image_dict = {'width' : width,
+                              'height': height,
+                              'ref_points': ref_points}
+
+            else:
+                image_dict['ref_points'] = ref_points
+
+            return image_dict, None
+
+def click_and_crop(event, x, y, flags, param):
+    # grab references to the global variables
+    global ref_point, image, rect
+
+    # if the left mouse button was clicked, record the starting
+    # (x, y) coordinates
+    if event == cv2.EVENT_LBUTTONDOWN:
+        ref_point = [(x, y)]
+
+        # check if event is moving a box
+        rect = check_moving(x,y)
+        if rect:
+            print(rect, ' selected')
+
+
+    # check to see if the left mouse button was released
+    elif event == cv2.EVENT_LBUTTONUP:
+        # record the ending (x, y) coordinates
+        ref_point.append((x, y))
+
+    elif len(ref_point) == 1 and event == cv2.EVENT_MOUSEMOVE:
+        image = clone.copy()
+        cv2.rectangle(image, ref_point[0], (x,y), (0,0,0), line_width)
+
+
+def check_moving(x,y):
+    rects = None
+
+    for rect in ref_points:
+        high_y = min(rect[0][1], rect[1][1])
+        low_y = max(rect[0][1], rect[1][1])
+        left_x = min(rect[0][0], rect[1][0])
+        right_x = max(rect[0][0], rect[1][0])
+
+        if (((left_x - line_width <= x <= left_x + line_width) or
+             (right_x - line_width <= x <= right_x + line_width)) and
+             (low_y - line_width >= y >= high_y + line_width)):
+            rects = rect
+            break
+
+        elif (((high_y <= y <= high_y + line_width) or
+              (low_y <= y <= low_y + line_width)) and
+              (left_x - line_width/2 <= x <= right_x + line_width/2)):
+            rects = rect
+            break
+
+
+    return rects
+
+if __name__ == '__main__':
+    image_source = 'ir_test.png'
+    tag_image(image_source, colors, {'width': 1280,
+                                     'height': 720,
+                                     'ref_points': [[(235, 305), (386, 462), 1],
+                                                   [(471, 346), (621, 521), 2],
+                                                   [(581, 220), (756, 341), 3]]
+                                    })
---
+++ loop_images.py
@@ -0,0 +1,54 @@
+import os
+import pickle
+import cv2
+
+from image_tagger import tag_image
+from image_vars import colors
+
+
+def get_previous_labels(directory):
+    pickle_path = os.path.join(directory, 'image_labels.pickle')
+
+    if os.path.isfile(pickle_path):
+        with open(pickle_path, 'rb') as handle:
+            image_labels = pickle.load(handle)
+    else:
+        image_labels = {}
+
+    return image_labels, pickle_path
+
+
+def save_pickle_file(pickle_path, image_labels):
+    with open(pickle_path, 'wb') as handle:
+        pickle.dump(image_labels, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print('Saved data')
+
+
+def loop_images(directory, save_freq=100):
+    cv2_window = cv2.namedWindow("image_window", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("image_window", 1000, 700)
+    cv2.moveWindow("image_window", 0,0)
+    image_labels, pickle_path = get_previous_labels(directory)
+
+    for i, image in enumerate(os.listdir(directory)):
+        image_path = os.path.join(directory,image)
+        previous_labels = image_labels.get(image_path, None)
+        print('previous labels: ', previous_labels)
+
+        image_dict, quit = tag_image(image_path, colors, previous_labels, "image_window")
+        if image_dict:
+            image_labels[image_path] = image_dict
+
+        if not i % save_freq:
+            save_pickle_file(pickle_path, image_labels)
+
+        if quit == 'quit':
+            cv2.destroyAllWindows()
+            break
+
+    if i % save_freq:
+        save_pickle_file(pickle_path, image_labels)
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    loop_images('test_images/', save_freq=5)
